35ef06d806e83173ab782106e1190b703a5fd6ee
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2023-09-12 12:46:15 +0200

    bug

--- controller.php
+++ controller.php
@@ -737,10 +737,10 @@ class Controller extends BlockController
             ]);
         }
 
-        usort($students, array($this, 'students_sorter'));
         $students = array_filter($students, function ($student) {
-            return $student["these_date_soutenance"] != "" && date_parse($student["these_date_soutenance"])["year"] <= date("Y")+1 && time() < strtotime($student["these_date_soutenance"]);
+            return $student["these_date_soutenance"] != "" && strtotime($student["these_date_soutenance"]) < strtotime("31-12-".($this->year+1)) && time() < strtotime($student["these_date_soutenance"]);
         });
+        usort($students, array($this, 'defense_sorter'));
 
         $byGroup = $this->group_by("these_ED_code", $students);
         foreach ($byGroup as &$valueByED) {
