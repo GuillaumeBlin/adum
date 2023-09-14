957d4e35e626af1eb1b12d4b215d4583f94f6e2b
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2023-09-14 13:20:00 +0200

    bug form

--- form.php
+++ form.php
@@ -2,12 +2,13 @@
 
 $parsing_types = array("doctors_of_the_year" => "Docteur.e.s de l'année en cours", "phd_proposal" => "Propositions de sujets de thèse", "annu" => "Annuaire des doctorant.e.s", "phd_defense_by_ed" => "Soutenances à venir", "members_annu" => "Annuaire des encadrant.e.s", "training_by_ed" => "Formations");
 $codes = array(""=>"CED","41" => "ED Droit", "42" => "ED Entreprise Economie Société", "40" => "ED Sciences Chimiques", "154" => "ED Sciences de la Vie et de la Santé", "304" => "ED Sciences et environnements", "209" => "ED Sciences Physiques et de l'Ingénieur", "545" => "ED Sociétés, Politique, Santé Publique", "39" => "ED Mathématiques et Informatique");
+$year_needed = array("doctors_of_the_year", "annu");
 ?>
 
 
 <div class="form-group">
 	<label class="control-label" for="parsing">Type d'information</label>
-	<select name="parsing" class="ccm-input-select">
+	<select id="parsing" name="parsing" class="ccm-input-select">
 		<?php foreach ($parsing_types as $key => $opt) { ?>
 			<option value="<?php echo $key; ?>" <?php if (strcmp($parsing, $key) === 0) { ?>selected<?php } ?>> <?php echo $opt; ?></option>
 		<?php } ?>
@@ -21,7 +22,7 @@ $codes = array(""=>"CED","41" => "ED Droit", "42" => "ED Entreprise Economie Soc
 		<?php } ?>
 	</select>
 	<br/><label class="control-label" for="year">Année affichée</label>
-	<select name="year" class="ccm-input-select">
+	<select id="year" name="year" class="ccm-input-select">
 		<?php for($i=2016;$i<date("Y")+1;$i++) { ?>
 			<option value="<?php echo $i; ?>" <?php if (strcmp($year, $i) === 0) { ?>selected<?php } ?>> <?php echo $i; ?> - <?php echo $i+1; ?></option>
 		<?php } ?>
@@ -38,3 +39,10 @@ $codes = array(""=>"CED","41" => "ED Droit", "42" => "ED Entreprise Economie Soc
 		<option value="EN" <?php if (strcmp($langage, "EN") === 0) { ?>selected<?php } ?>> Anglais</option>
 	</select>
 </div>
+
+<script>
+$("#parsing").change(function() {
+    var disabled = (this.value != "doctors_of_the_year" && this.value != "annu");
+    $("#year").prop("disabled", disabled);
+}).change(); //to trigger on load
+</script>
