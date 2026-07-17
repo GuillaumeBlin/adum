bf423c3ac5b3ffe3f2c84374fd9157f9d41ae4f6
Guillaume Blin <lemail2guillaume@gmail.com>
2026-07-17 09:52:42 +0200

    Update date comparison logic for student filtering

--- controller.php
+++ controller.php
@@ -930,8 +930,8 @@ class Controller extends BlockController
 
 
         $students = array_filter($students, function ($student) {
-            return ((strtotime("12 June 2026") <= strtotime($student["these_date_soutenance"])) && (strcmp($student["these_visibilite"],"") == 0));
-            #return ((strtotime("today") <= strtotime($student["these_date_soutenance"])) && (strcmp($student["these_visibilite"],"") == 0));
+            return (strtotime("-2 week") <= strtotime($student["these_date_soutenance"])) && (strcmp($student["these_visibilite"],"") == 0));
+            //return ((strtotime("today") <= strtotime($student["these_date_soutenance"])) && (strcmp($student["these_visibilite"],"") == 0));
         });
 
         usort($students, array($this, 'defense_sorter'));
